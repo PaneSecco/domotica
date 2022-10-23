d1ef062542b328e28247d3106119afaae537997b
Gatto lunatico <101564199+PaneSecco@users.noreply.github.com>
2022-10-23 22:59:09 +0200

    fine imlementazione classe termostato, da sistemare dubbio su ultima parte "stato funzionamento"

--- domotica/termostato.cs
+++ domotica/termostato.cs
@@ -12,17 +12,56 @@ namespace domotica
         private string produttore;
         private string modello;
         private int numeroserie;
+        private caldaia cal;
 
         public termostato(string produttore1, string modello1, int numeroserie1)
         {
             produttore= produttore1;
             modello= modello1;
             numeroserie= numeroserie1;
+
+            set_caldaia(cal);
         }
 
+        public void set_caldaia(caldaia cal1)
+        {
+            cal = cal1;
+        }
         public void set_stato()
+        {
+            cal.set_stato();
+        }
+
+        public void set_riscaldamento()
+        {
+            cal.set_riscaldamento();
+        }
+
+        public void set_temprisc(int temp)
+        {
+            cal.set_temprisc(temp);
+        }
+
+        public void set_tempacqua(int temp)
+        {
+            cal.set_tempacqua(temp);
+        }
+
+        public int get_temprisc()
+        {
+            return cal.get_temprisc();
+        }
+
+        public int get_tempacqua()
+        {
+            return cal.get_tempacqua();
+        }
+
+        /*      ???????????????
+        public void get_statofunz()
         {
 
         }
+        */
     }
 }
